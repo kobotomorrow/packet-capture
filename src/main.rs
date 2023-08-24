8aec724d90ed207649a2b084d1ec496973fe141e
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2023-08-24 15:52:08 +0900

    feat: select networkinterface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use log::error;
+use pnet::datalink;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        error!("Please provide a path to a file");
+        std::process::exit(1);
+    }
+    let interface_name = &args[1];
+
+    // インターフェースの選択
+    let interfaces = datalink::interfaces();
+    let interface = interfaces.into_iter().find(|iface|
+        iface.name == *interface_name
+    ).expect("Failed to get interface");
 }
